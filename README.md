1ea417829e04c097881063eaff84d5e16e0ef40e
Eddie Lin <elin@mozilla.com>
2015-07-31 07:27:28 +0800

    Initial implementation.

---
+++ README.md
@@ -0,0 +1,11 @@
+System - Bigger Unlocker
+---------------------------
+
+This is a small Firefox OS addon that enlarges the unlocker on lockscreen.
+
+You can [use the WebIDE to install it onto your device][webide], or someday you
+might find it in the Hackerplace!
+
+[webide]: https://developer.mozilla.org/en-US/Firefox_OS/Add-ons#Installation
+
+![Screenshot](https://raw.githubusercontent.com/elin-moco/fxos-addon-big-unlocker/master/screenshot.png)
---
+++ index.js
@@ -0,0 +1,26 @@
+(function () {
+
+  // If injecting into an app that was already running at the time
+  // the app was enabled, simply initialize it.
+  if (document.documentElement) {
+    initialize();
+  }
+
+  // Otherwise, we need to wait for the DOM to be ready before
+  // starting initialization since add-ons are usually (always?)
+  // injected *before* `document.documentElement` is defined.
+  else {
+    window.addEventListener('DOMContentLoaded', initialize);
+  }
+
+  function initialize() {
+
+    // Just a small shortcut to repeat myself less
+    var $$ = document.getElementById.bind(document);
+
+    // Remove existing control, for when this addon is re-run.
+    var containerEl = $$('lockscreen-icon-container');
+    containerEl.setAttribute('style', 'transform: scale(1.2, 1.5); bottom: 5%;');
+  }
+  
+}());
